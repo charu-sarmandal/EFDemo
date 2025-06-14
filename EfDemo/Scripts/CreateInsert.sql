9e6b16422b3839ffa86e4e32eca9f623c144f57f
charu <charu.sarmandal12@gmail.com>
2025-06-14 14:02:46 +0530

    DB Scripts

---
+++ EfDemo/Scripts/CreateInsert.sql
@@ -0,0 +1,15 @@
+use DairyDB
+
+--create table dbo.Products(Id int primary key, PrdName varchar(100),PrdType varchar(100),  IsActive bit, CreatedDate DateTime)
+--insert into dbo.Products(Id,PrdName,PrdType,IsActive,CreatedDate) values (1,'Milk','Regular',1, GETDATE())
+--insert into dbo.Products(Id,PrdName,PrdType,IsActive,CreatedDate) values (2,'Milk','Gold',1, GETDATE())
+--insert into dbo.Products(Id,PrdName,PrdType,IsActive,CreatedDate) values (3,'Milk','Double Toned',1, GETDATE())
+--insert into dbo.Products(Id,PrdName,PrdType,IsActive,CreatedDate) values (4,'Milk','Toned',1, GETDATE())
+--insert into dbo.Products(Id,PrdName,PrdType,IsActive,CreatedDate) values (5,'Milk','FCM',1, GETDATE())
+
+select * from dbo.products
+
+
+--create table Customers(Id int primary key, CustFirstName varchar(100),CustLastName varchar(100),Age int,  IsActive bit, CreatedDate DateTime)
+
+
