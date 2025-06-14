606fe5121a2f7fec4516d8c892a232a7e55905fb
charu <charu.sarmandal12@gmail.com>
2025-06-14 14:01:04 +0530

    EntityFramework settings

--- EfDemo/DairyDbContext.cs
+++ EfDemo/DairyDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace EfDemo
+﻿
+using EfDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfDemo
 {
-    public class DairyDbContext 
+    public class DairyDbContext : DbContext
     {
+        public DairyDbContext(DbContextOptions<DairyDbContext> options) : base(options) { }
+
+        public DbSet<ProductModel> Products { get; set; }
     }
 }
--- EfDemo/Models/ProductModel.cs
+++ EfDemo/Models/ProductModel.cs
@@ -2,5 +2,15 @@
 {
     public class ProductModel
     {
+
+        public int Id { get; set; }
+
+        public string  PrdName{ get; set; }
+
+        public string PrdType { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedDate { get; set; }
     }
 }
